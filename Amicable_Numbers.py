bab6f8eb8ca9e6d6605c926a5eb8e095ec80e3af
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-05-20 21:51:36 +0530

    def sum_proper_divisors(n):     divisors=[1]     for i in range(2,int(n**0.5)+1):         if n%i==0:             divisors.append(i)             if i!=n//i:                 divisors.append(n//i)     return sum(divisors) def are_amicable(a,b):     sum_a=sum_proper_divisors(a)     sum_b=sum_proper_divisors(b)     return sum_a==b and sum_b==a num1=int(input()) num2=int(input()) if are_amicable(num1,num2):     print("Amicable") else:     print("Not Amicable")

---
+++ Amicable_Numbers.py
@@ -0,0 +1,19 @@
+def sum_proper_divisors(n):
+    divisors=[1]
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            divisors.append(i)
+            if i!=n//i:
+                divisors.append(n//i)
+    return sum(divisors)
+def are_amicable(a,b):
+    sum_a=sum_proper_divisors(a)
+    sum_b=sum_proper_divisors(b)
+    return sum_a==b and sum_b==a
+num1=int(input())
+num2=int(input())
+if are_amicable(num1,num2):
+    print("Amicable")
+else:
+    print("Not Amicable")
+    
