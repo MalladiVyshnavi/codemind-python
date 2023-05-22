f47edc62c1b8f6978bd5182bc5f8a77252f835a7
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-05-22 06:45:47 +0530

    def count_odd_betweenodd(arr):     count=0     for i in range(1,len(arr)-1):         if arr[i]%2==1 and arr[i-1]%2==1 and arr[i+1]%2==1:             count+=1     return count N=int(input()) arr=list(map(int,input().split())) result=count_odd_betweenodd(arr) print(result)

---
+++ count_the_add_numbers_inbetween_odd_numbers.py
@@ -0,0 +1,10 @@
+def count_odd_betweenodd(arr):
+    count=0
+    for i in range(1,len(arr)-1):
+        if arr[i]%2==1 and arr[i-1]%2==1 and arr[i+1]%2==1:
+            count+=1
+    return count
+N=int(input())
+arr=list(map(int,input().split()))
+result=count_odd_betweenodd(arr)
+print(result)
